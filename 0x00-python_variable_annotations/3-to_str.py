7e51d219392fbc9bd1c699c1e9e345eb31126c4f
Xolelwa34 <ciliaswekile@gmail.com>
2024-10-10 12:56:26 -0700

    Basic annotations to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Returns the string representation of the float
+"""
+
+
+def to_str(n: float) -> str:
+    """ 
+    function that cast to float to string
+    """
+    return str(n)
