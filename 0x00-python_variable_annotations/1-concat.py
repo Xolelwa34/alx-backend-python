812026fab7043a9b92fdff67e6c0ee90e37d705e
Xolelwa34 <ciliaswekile@gmail.com>
2024-10-10 11:00:13 -0700

    Concatenates

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+concatenates all of the values in its argument list
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    function to concatenates two strings
+    """
+    return str1 + str2
