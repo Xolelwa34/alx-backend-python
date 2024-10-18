1c7ab490e57ec40d00590be8028e14aa5afe2c7a
Xolelwa34 <ciliaswekile@gmail.com>
2024-10-18 08:16:54 -0700

    async comprehension fixed

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,28 +1,9 @@
 #!/usr/bin/env python3
-""" Async generator from the previous task """
-import asyncio
+"""Generates a list fro async comprehension"""
 from typing import List
-
-
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> List[float]:
-    """
-        async generates numbers with comprehension
-
-        Args:
-            void
-
-        Return:
-            float random numbers
-    """
-    async def async_comprehension():
-    result = [item async for item in async_generator()]
-    return result[:10]
-
-async def main():
-    result = await async_comprehension()
-    print(result)
-
-asyncio.run(main())
+async def async_comprehension() -> List[float]: 
+    """ Generates numbers"""
+    return [_ async for _ in async_generator()]
